d5f1c928de5b199549ca8dc4f5305cfbc5fe49f5
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-03 16:43:56 +0530

    updated App.js | added Routes

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./component/home";
+
 function App() {
-  return <h1>Working</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
