c7f9fecec48aa52f8533f738a7620f953352174a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-04 01:04:25 +0530

    updated bar.jsx

--- server/client/src/components/main/bar.jsx
+++ server/client/src/components/main/bar.jsx
@@ -1,14 +1,32 @@
+import SmallBox from "../common/smallBox";
+import EventBox from "../common/eventBox";
 import BigBox from "../common/bigBox";
 import "../../css/bar.css";
 
-const Bar = ({ data }) => {
+const Bar = ({ data, isBigBox, onAdd, isEventBox }) => {
   return (
     <div className="bar">
-      {data.map((content, index) => (
-        <BigBox key={index} content={content} />
-      ))}
+      {data.map((content, index) => {
+        if (isBigBox) return <BigBox key={index} content={content} />;
+        const { isClick, isDrag } = content;
+        return (
+          <SmallBox
+            content={content}
+            key={index}
+            isClick={isClick}
+            isDrag={isDrag}
+            onAdd={onAdd}
+          />
+        );
+      })}
+      {isEventBox && <EventBox onAdd={onAdd} />}
     </div>
   );
+};
+
+Bar.defaultProps = {
+  isBigBox: true,
+  isEventBox: false,
 };
 
 export default Bar;
