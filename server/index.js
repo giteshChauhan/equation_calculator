b59860f83fc3568c61802a4f75f685bbfe57d552
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-03 14:55:54 +0530

    update index.js | added alphabet route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
+const { urlencoded, json } = require("express");
 const mongoose = require("mongoose");
 const express = require("express");
 const config = require("config");
 const app = express();
+
+const alphabet = require("./route/alphabet");
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use("/api/alphabet/", alphabet);
 
 mongoose
   .connect(config.get("db"))
