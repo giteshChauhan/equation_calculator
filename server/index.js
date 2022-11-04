2ad5ad71b097ec9d66dad480ee9152a2f262a11c
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-04 08:22:33 +0530

    update index.js | added cors() middleware

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const { urlencoded, json } = require("express");
 const mongoose = require("mongoose");
 const express = require("express");
 const config = require("config");
+const cors = require("cors");
 const app = express();
 
 const alphabet = require("./route/alphabet");
 
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use("/api/alphabet/", alphabet);
