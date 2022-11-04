fd0781175a7604844a505c02e49d0a518d6e3051
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-04 08:37:05 +0530

    production ready

--- server/index.js
+++ server/index.js
@@ -17,5 +17,13 @@ mongoose
   .then(() => console.log("Connected to db..."))
   .catch((error) => console.log(`Cannot connect...${error}`));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 let port = process.env.PORT || 3900;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
